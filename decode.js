858dbba30199ede981982f6f4d68a6619c9ab1e8
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-05-02 17:49:59 -0700

    fix: decode before parsing

--- decode.js
+++ decode.js
@@ -1,9 +1,10 @@
 export function slackDecode (encoded) {
-    let decoded = encoded.replace(/\&amp\;/g, '&').replace(/\&lt\;/g, '<').replace(/\&gt\;/g, '>');
-    let matches = decoded.match(/<.*?>/g) || [];
+    let decoded = encoded;
+    let matches = decoded.match(/\&lt\;.*?\&gt\;/g) || [];
     for (const match of matches) {
         let value = match.substring(1, match.length - 1).split('|').reverse()[0];
         decoded = decoded.replace(match, value);
     }
+    decoded = decoded.replace(/\&amp\;/g, '&').replace(/\&lt\;/g, '<').replace(/\&gt\;/g, '>');
     return decoded;
 }
